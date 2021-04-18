eaa4b58fb6792c3c9949ddf0023671744098c692
Nícalo Ribeiro <nicalo63@gmail.com>
2021-04-18 18:34:35 -0300

    Barra verde por falsificacao

--- Calculadora/src/main/java/TDD/CalculadoraCDB.java
+++ Calculadora/src/main/java/TDD/CalculadoraCDB.java
@@ -1,5 +1,13 @@
 package TDD;
 
 public class CalculadoraCDB {
+
+    public void calcularRendimentoBruto(int dias, double capital, double taxaJurosAnual){
+   
+    } 
+
+    public double getRendimentoBruto(){
+        return 13.97;
+    }
     
 }
