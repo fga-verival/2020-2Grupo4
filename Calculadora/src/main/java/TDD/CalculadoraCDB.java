cde434d103bc789f255ab3464ad08761d6797fbf
Nícalo Ribeiro <nicalo63@gmail.com>
2021-04-18 18:38:15 -0300

    Barra verde apos implementacao

--- Calculadora/src/main/java/TDD/CalculadoraCDB.java
+++ Calculadora/src/main/java/TDD/CalculadoraCDB.java
@@ -2,12 +2,15 @@ package TDD;
 
 public class CalculadoraCDB {
 
+    private double rendimentoBruto;
+
     public void calcularRendimentoBruto(int dias, double capital, double taxaJurosAnual){
-   
+        double rendimento = capital * ((double)dias/365) * (taxaJurosAnual/100);
+        this.rendimentoBruto = Math.round(rendimento * 100.0) / 100.0;
     } 
 
     public double getRendimentoBruto(){
-        return 13.97;
+        return this.rendimentoBruto;
     }
     
 }
