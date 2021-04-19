f373367cdeebabfd0ed3d3be1e41a172ea4e9957
MaiconMares <maiconmaresunb@gmail.com>
2021-04-18 21:26:42 -0300

    Obtendo barra verde apos implementacao para a unidade.

--- Calculadora/src/main/java/TDD/CalculadoraCDB.java
+++ Calculadora/src/main/java/TDD/CalculadoraCDB.java
@@ -3,6 +3,7 @@ package TDD;
 public class CalculadoraCDB {
 
     private double rendimentoBruto;
+    private double impostoDeRenda;
 
     public void calcularRendimentoBruto(int dias, double capital, double taxaJurosAnual){
         double rendimento = capital * ((double)dias/365) * (taxaJurosAnual/100);
@@ -14,11 +15,18 @@ public class CalculadoraCDB {
     }
 
     public void calcularImpostoDeRenda(int prazo) {
-        
+        if (prazo > 0 && prazo <= 180)
+            this.impostoDeRenda = 0.225f;
+        else if (prazo >= 181 && prazo <= 360)
+            this.impostoDeRenda = 0.200f;
+        else if (prazo >= 361 && prazo <= 720)
+            this.impostoDeRenda = 0.175f;
+        else
+            this.impostoDeRenda = 0.150f;
     }
 
     public double getImpostoDeRenda() {
-        return 0.225f;
+        return this.impostoDeRenda;
     }
     
 }
