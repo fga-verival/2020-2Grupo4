f13c85103e94dedd9f4d1d11119ed0e04385b184
MaiconMares <maiconmaresunb@gmail.com>
2021-04-18 21:12:41 -0300

    Obtendo barra vermelha por falta de implementacao da unidade.

--- Calculadora/src/test/java/TDD/CalculadoraCDBTest.java
+++ Calculadora/src/test/java/TDD/CalculadoraCDBTest.java
@@ -1,4 +1,5 @@
 package TDD;
+
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -7,25 +8,31 @@ public class CalculadoraCDBTest {
     private CalculadoraCDB calculadora;
 
     @Before
-    public void setup(){
+    public void setup() {
         calculadora = new CalculadoraCDB();
     }
 
     @Test
-    public void TestarCalculoRendimentoBruto(){
+    public void TestarCalculoRendimentoBruto() {
         calculadora.calcularRendimentoBruto(60, 1000, 8.5);
         assertEquals(13.97, calculadora.getRendimentoBruto(), 0);
     }
 
     @Test
-    public void TestarCalculoRendimentoBruto2(){
+    public void TestarCalculoRendimentoBruto2() {
         calculadora.calcularRendimentoBruto(120, 500, 8.0);
         assertEquals(13.15, calculadora.getRendimentoBruto(), 0);
     }
-    
+
     @Test
-    public void TestarCalculoRendimentoBruto3(){
+    public void TestarCalculoRendimentoBruto3() {
         calculadora.calcularRendimentoBruto(240, 3000, 9.0);
         assertEquals(177.53, calculadora.getRendimentoBruto(), 0);
+    }
+
+    @Test
+    public void testarImpostoDeRenda() {
+        calculadora.calcularImpostoDeRenda(180);
+        assertEquals(0.225f, getImpostoDeRenda(), 0);
     }
 }
