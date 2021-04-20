8593dfbac05d6322238e0fa31df491f4cd1d2eda
Filipe Machado <filipemachado@datapolicy.co>
2021-04-20 19:03:13 -0300

    refactor functions names to match source code

--- Calculadora/src/test/java/TDD/CalculadoraCDBTest.java
+++ Calculadora/src/test/java/TDD/CalculadoraCDBTest.java
@@ -5,47 +5,47 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CalculadoraCDBTest {
-    private CalculadoraCDB calculadora;
+    private CalculadoraCDB calculator;
 
     @Before
     public void setup() {
-        calculadora = new CalculadoraCDB();
+        calculator = new CalculadoraCDB();
     }
 
     @Test
-    public void TestarCalculoRendimentoBruto() {
-        calculadora.calcularRendimentoBruto(60, 1000, 8.5);
-        assertEquals(13.97, calculadora.getRendimentoBruto(), 0);
+    public void TestGrossIncomeCalculation() {
+        calculator.calculateGrossIncome(60, 1000, 8.5);
+        assertEquals(13.97, calculator.getGrossIncome(), 0);
     }
 
     @Test
-    public void TestarCalculoRendimentoBruto2() {
-        calculadora.calcularRendimentoBruto(120, 500, 8.0);
-        assertEquals(13.15, calculadora.getRendimentoBruto(), 0);
+    public void TestGrossIncomeCalculation2() {
+        calculator.calculateGrossIncome(120, 500, 8.0);
+        assertEquals(13.15, calculator.getGrossIncome(), 0);
     }
 
     @Test
-    public void TestarCalculoRendimentoBruto3() {
-        calculadora.calcularRendimentoBruto(240, 3000, 9.0);
-        assertEquals(177.53, calculadora.getRendimentoBruto(), 0);
+    public void TestGrossIncomeCalculation3() {
+        calculator.calculateGrossIncome(240, 3000, 9.0);
+        assertEquals(177.53, calculator.getGrossIncome(), 0);
     }
 
     @Test
-    public void testarImpostoDeRenda() {
-        calculadora.calcularImpostoDeRenda(180);
-        assertEquals(0.225f, calculadora.getImpostoDeRenda(), 0);
+    public void testIncomeTax() {
+        calculator.calculateIncomeTax(180);
+        assertEquals(0.225f, calculator.getIncomeTax(), 0);
     }
 
     @Test
-    public void testarImpostoDeRenda2() {
-        calculadora.calcularImpostoDeRenda(240);
-        assertEquals(0.20f, calculadora.getImpostoDeRenda(), 0);
+    public void testIncomeTax2() {
+        calculator.calculateIncomeTax(240);
+        assertEquals(0.20f, calculator.getIncomeTax(), 0);
     }
 
     @Test
-    public void testarImpostoDeRenda3() {
-        calculadora.calcularImpostoDeRenda(700);
-        calculadora.calcularImpostoDeRenda(800);
-        assertEquals(0.15f, calculadora.getImpostoDeRenda(), 0);
+    public void testIncomeTax3() {
+        calculator.calculateIncomeTax(700);
+        calculator.calculateIncomeTax(800);
+        assertEquals(0.15f, calculator.getIncomeTax(), 0);
     }
 }
